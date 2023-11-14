941df66c3dcd2e2ee397807bc87040c02a6cb615
penin21 <130118340+penin21@users.noreply.github.com>
2023-11-15 05:51:06 +0900

    Update 1.js

---
+++ 1.js
@@ -0,0 +1,514 @@
+var gridData = [
+    {
+        "name": '양식에 맞춰 여기에 복붙하세요',
+        "word": "https://s3.ap-northeast-2.amazonaws.com/file.cedulearn.com/dalle/conclude_42_2.png"
+    },
+    {
+        "id": 5871,
+        "word": " archeology"
+    },
+    {
+        "id": 5608,
+        "word": " askew"
+    },
+    {
+        "id": 7920,
+        "word": " cursive"
+    },
+    {
+        "id": 2835,
+        "word": " fungi"
+    },
+    {
+        "id": 5982,
+        "word": " gutless"
+    },
+    {
+        "id": 3844,
+        "word": " incinerator"
+    },
+    {
+        "id": 4410,
+        "word": " monastery"
+    },
+    {
+        "id": 4400,
+        "word": " monkish"
+    },
+    {
+        "id": 4153,
+        "word": " narcosis"
+    },
+    {
+        "id": 2514,
+        "word": " preadolescent "
+    },
+    {
+        "id": 2518,
+        "word": " puerility"
+    },
+    {
+        "id": 6280,
+        "word": " raggedness"
+    },
+    {
+        "id": 7808,
+        "word": " revolving"
+    },
+    {
+        "id": 3634,
+        "word": " savior"
+    },
+    {
+        "id": 4699,
+        "word": " stridence"
+    },
+    {
+        "id": 11298,
+        "word": " tactician"
+    },
+    {
+        "id": 4969,
+        "word": " vexatious"
+    },
+    {
+        "id": 11281,
+        "word": "a lot of"
+    },
+    {
+        "id": 12928,
+        "word": "a.m."
+    },
+    {
+        "id": 295,
+        "word": "abandon"
+    },
+    {
+        "id": 3713,
+        "word": "abandoned"
+    },
+    {
+        "id": 296,
+        "word": "abandonment"
+    },
+    {
+        "id": 10044,
+        "word": "abase"
+    },
+    {
+        "id": 10045,
+        "word": "abasement"
+    },
+    {
+        "id": 6908,
+        "word": "abate"
+    },
+    {
+        "id": 6909,
+        "word": "abatement"
+    },
+    {
+        "id": 4134,
+        "word": "abbreviate"
+    },
+    {
+        "id": 12917,
+        "word": "abbreviation"
+    },
+    {
+        "id": 12496,
+        "word": "abdicate"
+    },
+    {
+        "id": 12787,
+        "word": "abdication"
+    },
+    {
+        "id": 12788,
+        "word": "abdicator"
+    },
+    {
+        "id": 9420,
+        "word": "abdomen"
+    },
+    {
+        "id": 9421,
+        "word": "abdominal"
+    },
+    {
+        "id": 10873,
+        "word": "abduct"
+    },
+    {
+        "id": 10874,
+        "word": "abduction"
+    },
+    {
+        "id": 1922,
+        "word": "aberrant"
+    },
+    {
+        "id": 1923,
+        "word": "aberration"
+    },
+    {
+        "id": 13328,
+        "word": "abhor"
+    },
+    {
+        "id": 13330,
+        "word": "abhorrence"
+    },
+    {
+        "id": 13329,
+        "word": "abhorrent"
+    },
+    {
+        "id": 13073,
+        "word": "abide by"
+    },
+    {
+        "id": 14095,
+        "word": "abiding"
+    },
+    {
+        "id": 7234,
+        "word": "ability"
+    },
+    {
+        "id": 10084,
+        "word": "abject"
+    },
+    {
+        "id": 10086,
+        "word": "abjection"
+    },
+    {
+        "id": 13212,
+        "word": "abjuration"
+    },
+    {
+        "id": 13211,
+        "word": "abjure"
+    },
+    {
+        "id": 3797,
+        "word": "ablaze"
+    },
+    {
+        "id": 7911,
+        "word": "able"
+    },
+    {
+        "id": 12677,
+        "word": "abnegation"
+    },
+    {
+        "id": 512,
+        "word": "abnormal"
+    },
+    {
+        "id": 3419,
+        "word": "abnormality"
+    },
+    {
+        "id": 6805,
+        "word": "aboard"
+    },
+    {
+        "id": 6570,
+        "word": "abolish"
+    },
+    {
+        "id": 6571,
+        "word": "abolishment"
+    },
+    {
+        "id": 14093,
+        "word": "abolition"
+    },
+    {
+        "id": 15951,
+        "word": "abominable"
+    },
+    {
+        "id": 14639,
+        "word": "abominate"
+    },
+    {
+        "id": 5710,
+        "word": "aboriginal"
+    },
+    {
+        "id": 12134,
+        "word": "aborigine"
+    },
+    {
+        "id": 6817,
+        "word": "abort"
+    },
+    {
+        "id": 6820,
+        "word": "abortion"
+    },
+    {
+        "id": 6821,
+        "word": "abortive"
+    },
+    {
+        "id": 6436,
+        "word": "abound"
+    },
+    {
+        "id": 12499,
+        "word": "about"
+    },
+    {
+        "id": 11386,
+        "word": "above"
+    },
+    {
+        "id": 10644,
+        "word": "abrade"
+    },
+    {
+        "id": 9632,
+        "word": "abrasion"
+    },
+    {
+        "id": 10645,
+        "word": "abrasive"
+    },
+    {
+        "id": 5726,
+        "word": "abreast"
+    },
+    {
+        "id": 14797,
+        "word": "abridge"
+    },
+    {
+        "id": 15376,
+        "word": "abridgment"
+    },
+    {
+        "id": 6806,
+        "word": "abroad"
+    },
+    {
+        "id": 8216,
+        "word": "abrogate"
+    },
+    {
+        "id": 8217,
+        "word": "abrogation"
+    },
+    {
+        "id": 1869,
+        "word": "abrupt"
+    },
+    {
+        "id": 1871,
+        "word": "abruptly"
+    },
+    {
+        "id": 6043,
+        "word": "abscess"
+    },
+    {
+        "id": 3351,
+        "word": "abscond"
+    },
+    {
+        "id": 808,
+        "word": "absence"
+    },
+    {
+        "id": 188,
+        "word": "absent"
+    },
+    {
+        "id": 2887,
+        "word": "absent-minded"
+    },
+    {
+        "id": 809,
+        "word": "absently"
+    },
+    {
+        "id": 200,
+        "word": "absolute"
+    },
+    {
+        "id": 817,
+        "word": "absolutely"
+    },
+    {
+        "id": 5939,
+        "word": "absolution"
+    },
+    {
+        "id": 5938,
+        "word": "absolve"
+    },
+    {
+        "id": 260,
+        "word": "absorb"
+    },
+    {
+        "id": 261,
+        "word": "absorption"
+    },
+    {
+        "id": 1648,
+        "word": "abstain"
+    },
+    {
+        "id": 14888,
+        "word": "abstainer"
+    },
+    {
+        "id": 2981,
+        "word": "abstemious"
+    },
+    {
+        "id": 9699,
+        "word": "abstention"
+    },
+    {
+        "id": 9698,
+        "word": "abstentious"
+    },
+    {
+        "id": 14802,
+        "word": "abstergent"
+    },
+    {
+        "id": 9697,
+        "word": "abstinent"
+    },
+    {
+        "id": 1721,
+        "word": "abstract"
+    },
+    {
+        "id": 1722,
+        "word": "abstraction"
+    },
+    {
+        "id": 10145,
+        "word": "abstruse"
+    },
+    {
+        "id": 10147,
+        "word": "abstrusely"
+    },
+    {
+        "id": 10146,
+        "word": "abstruseness"
+    },
+    {
+        "id": 346,
+        "word": "absurd"
+    },
+    {
+        "id": 6437,
+        "word": "abundance"
+    },
+    {
+        "id": 393,
+        "word": "abundant"
+    },
+    {
+        "id": 6112,
+        "word": "abuse"
+    },
+    {
+        "id": 6121,
+        "word": "abusive"
+    },
+    {
+        "id": 2667,
+        "word": "academic"
+    },
+    {
+        "id": 2668,
+        "word": "academy"
+    },
+    {
+        "id": 9357,
+        "word": "accede"
+    },
+    {
+        "id": 14808,
+        "word": "accelerant"
+    },
+    {
+        "id": 510,
+        "word": "accelerate"
+    },
+    {
+        "id": 511,
+        "word": "acceleration"
+    },
+    {
+        "id": 14100,
+        "word": "accelerator"
+    },
+    {
+        "id": 3994,
+        "word": "accent"
+    },
+    {
+        "id": 4201,
+        "word": "accentuate"
+    },
+    {
+        "id": 13090,
+        "word": "accentuation"
+    },
+    {
+        "id": 3573,
+        "word": "accept"
+    },
+    {
+        "id": 3579,
+        "word": "acceptable "
+    },
+    {
+        "id": 3581,
+        "word": "acceptance"
+    },
+    {
+        "id": 6776,
+        "word": "access"
+    },
+    {
+        "id": 9391,
+        "word": "accessibility"
+    },
+    {
+        "id": 6778,
+        "word": "accessible"
+    },
+    {
+        "id": 9358,
+        "word": "accession"
+    },
+    {
+        "id": 7962,
+        "word": "accessory"
+    },
+    {
+        "id": 905,
+        "word": "accident"
+    },
+    {
+        "id": 908,
+        "word": "accidental"
+    },
+    {
+        "id": 909,
+        "word": "accidentally"
+    }
+]
